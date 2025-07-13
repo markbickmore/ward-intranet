d1177e90e706dba1d90379c4f3acab8b0bb5845d
markbickmore <mark@herohomeshumboldt.com>
2025-07-13 10:40:06 -0700

    Update EmergencyPrep.js

--- src/EmergencyPrep.js
+++ src/EmergencyPrep.js
@@ -1,1 +1,4 @@
 EmergencyPrep.js
+Likely integrates your new ward emergency plan.
+➡️ Build neighborhood zones, assign zone leaders, link to maps.
+➡️ Use collapsible sections (Comms Plan, Supply List, Assembly Points).
