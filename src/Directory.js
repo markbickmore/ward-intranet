58a46c9f3276745a522bf78e7bcf433080ebb24b
markbickmore <mark@herohomeshumboldt.com>
2025-07-13 10:39:42 -0700

    Update Directory.js

--- src/Directory.js
+++ src/Directory.js
@@ -1,1 +1,4 @@
 Directory.js
+Your LDS directory app clone (with better privacy).
+➡️ Add search + filter by auxiliary, priesthood office, youth/adult.
+➡️ Add emergency contact data fields (with permissions).
