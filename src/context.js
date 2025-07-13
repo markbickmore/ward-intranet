f0582f7d1b363288d9713c11cd582b53e286861f
markbickmore <mark@herohomeshumboldt.com>
2025-07-13 11:01:52 -0700

    Update context.js

--- src/context.js
+++ src/context.js
@@ -1,1 +1,15 @@
-context
+import React, { createContext, useState, useContext } from 'react';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [userRole, setUserRole] = useState('bishopric'); // Change as needed
+
+  return (
+    <AuthContext.Provider value={{ userRole, setUserRole }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
